c548b418b61dc33689b02d4c49e60e53e4d303bc
Mangala Madhu <mangalamadhu6302@gmail.com>
2022-03-24 10:37:17 +0530

    wheather-appfiles

---
+++ app.js
@@ -0,0 +1,15 @@
+// const request = require("request");
+// const url =
+//   "http://api.weatherstack.com/current?access_key=53bf263c9b37c3663ca74528e6513dda&query=37.8267,-122.4233&units=s";
+// request({ url: url, json: true }, (error, responce) => {
+//   //parses the response body as JSON.
+//   //================================
+//   //console.log(responce.body);
+//   //console.log(JSON.parse(responce.body));
+//   // const jsondata = JSON.parse(responce.body);
+//   // console.log(jsondata.current);
+//   console.log(
+//     `current is ${responce.body.current.temperature} and the rains fall chances is ${responce.body.current.precip}`
+//   );
+// });
+console.log("hey");
